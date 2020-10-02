b463e7957924f12656a9b40a5fec3ab1f1c6704e
artembelov <belov.artem@phystech.edu>
2020-10-02 21:22:30 +0300

    Задача F, полное решение

--- Contest4/solF.cpp
+++ Contest4/solF.cpp
@@ -16,7 +16,31 @@
  * =====================================================================================
  */
 #include <stdlib.h>
-#include <>
+#include <iostream>
+#include <vector>
+#include <stack>
 
+int sum(std::stack<int> stck){
+	int sum = 0;
+	while (stck.size()!=0){
+		sum += stck.top();
+		stck.pop();
+	}
+	return sum;
+}
 
+int main(){
+	int N;
+	std::cin >> N;
+	std::vector<std::stack<int> > vec(N); 
+	for (int i=0;i<N;i++){
+		int a,b,c;
+		std::cin >> a >> b >> c;
+		vec[a].push(c);
+	}
+	for (int i=0; i<N; i++){
+		if (vec[i].size() == 0){continue;}
+		std::cout << i <<  ' ' << vec[i].size() << ' ' << sum(vec[i]) << std::endl;
+	}
+}
 
