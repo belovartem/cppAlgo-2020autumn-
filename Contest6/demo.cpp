03d7c9625c87afecb7bd55a5e699ee358a20ee27
artembelov <belov.artem@phystech.edu>
2020-10-10 10:54:10 +0300

    задача на детерминированную машину для пред. контеста

---
+++ Contest6/demo.cpp
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  demo.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10.10.2020 09:52:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+
+
+int main(){
+	unsigned from, to; char c;
+	std::vector<std::vector<std::vector<unsigned>>> Automaton(61, std::vector<std::vector<unsigned>>(25));
+	
+	Automaton[from][c - 'a'].push_back(ro);
+//-----------------------------------------------
+	std::unordered_set<unsigned> reach;
+	reach.insert(0);
+//-----------------------------------------------
+	std::unordered_set<unsigned> reach;
+	
+	for (auto s : reach){
+		for (auto s : M[s][cc - 'a']) {
+			new_reach.insert(s);
+		}
+	}
+	reach.swap(new_reach);
+//-----------------------------------------------
+	
+
+}
