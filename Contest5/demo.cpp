ba0c7e8a6d956164d275a8c70fce02e4d614ef72
artembelov <belov.artem@phystech.edu>
2020-10-10 09:36:48 +0300

    Заготовочка для задачи А

---
+++ Contest5/demo.cpp
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  demo.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10.10.2020 09:23:51
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <string>
+
+int main(){
+	std::string s;
+	std::getline(s);
+
+	//
+	//
+	
+	while (true){
+		std::cin >> s;
+		if (1 == s.length()){
+			switch(s[0]){
+				case:
+				case:
+				default:
+					stoi(s);
+			}
+		}
+	}
+	return 0;
+}
+
