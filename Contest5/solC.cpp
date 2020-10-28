2462094dbc6e431330860d18725f0dc980699beb
artembelov <belov.artem@phystech.edu>
2020-10-28 13:01:35 +0300

    Привел все в более аккуратный вид, создал класс автомата

--- Contest5/solC.cpp
+++ Contest5/solC.cpp
@@ -24,48 +24,88 @@
 
 
 struct Point {
-	bool is_termination;
+	bool is_termination = false;
 	std::map<char, Point * > move_map;
 };
 
+class Automaton {
+	private:
+	std::vector<Point *> point;
+	Point * current_point;
+
+	public:
+	Automaton(int A, int N){
+		for (int i=0; i<N; i++){
+			Point * p = new Point; 
+			Point * fantom_point = new Point;
+
+			for (int j=0; j<A; j++){
+				fantom_point->move_map.insert(std::pair<char, Point * >('a'+j, fantom_point));
+				p->move_map.insert(std::pair<char, Point * >('a'+j, fantom_point));
+			}
+			point.push_back(p);
+		}
+	}
+
+	void set_termination(int a){
+		point[a]->is_termination = true;
+	}
+
+	void set_termination_map(){
+		int T;
+		std::cin >> T;
+		for (int i=0; i<T; i++){
+			int a;
+			std::cin >> a;
+			set_termination(a);
+		}		
+	}
+
+	void set_motion(int a,int b, char c){
+		point[a]->move_map[c] = point[b];
+	}
+
+	void set_motion_map(){
+		int M;
+		std::cin >> M;
+		for (int i=0; i<M; i++){
+			int a,b;
+			char c;
+			std::cin >> a >> b >> c;
+			set_motion(a,b,c);
+		}
+	}
+
+	void set_map(){
+		set_motion_map();
+		set_termination_map();
+	}
+
+	bool is_string_stsf(std::string str){
+		current_point = point[0];
+		for (int j=0; j<str.size(); j++){
+			char c = str[j];
+			current_point = current_point->move_map[c];
+		}
+		return current_point->is_termination;
+	}
+
+};
+
 int main(){
-	int A, M, N;
-	std::cin >> A >> N >> M;
-	std::vector<Point *> aut;
-	for (int i=0; i<N; i++){
-		Point * point = new Point;
-		point->is_termination = false;
-		std::map<char, Point * > move_map;
-		point->move_map = move_map;
-		aut.push_back(point);
-	}
-
-	for (int i=0; i<M; i++){
-		int a, b;
-		char c;
-		std::cin >> a >> b >> c;
-		aut[a]->move_map.insert(std::pair<char, Point *>(c, aut[b]));
-	}
-
-	int T;
-	std::cin >> T;
-	for (int i=0; i<T; i++){
-		int a;
-		std::cin >> a;
-		aut[a]->is_termination = true;
-	}
+	int A, N;
+	std::cin >> A >> N;
 	
+	Automaton * autom = new Automaton(A,N); 
+	
+	autom->set_map();
+
 	int K;
 	std::cin >> K;
 	std::string str;
 	str.reserve(8192);
 	for (int i=0; i<K; i++){
-		Point *p = aut[0];
 		std::cin >> str;
-		for (int j=0; j<str.size(); j++){
-			char c = str[j];
-			p = p->move_map[c];
-		}
-		std::cout << p->is_termination << std::endl;
+		std::cout << autom->is_string_stsf(str) << std::endl;
 	}
 }
